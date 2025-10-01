4e32f44624ed169751654503a31fbb75a909e011
Ingyne <ingynephyu.mm@gmail.com>
2025-10-01 09:02:27 +0630

    Initial Setup Continuous Integration

---
+++ sdlcEclipse/src/sdlcEclipse/Hellosdlc.java
@@ -0,0 +1,10 @@
+package sdlcEclipse;
+
+public class Hellosdlc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Continous integration");
+	}
+
+}
